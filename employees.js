dcc8db1b76ea05a797916bb4bdd4e5001e2623de
jorge <jorgearteaga8936@gmail.com>
2015-12-25 14:46:13 -0400

    using bootstrap, search filters added

--- employees.js
+++ employees.js
@@ -1,6 +1,6 @@
 [
-  { "firstName": "pepe", "lastName": "smith", "age": 35, "salary": 60000 },
-  { "firstName": "mario", "lastName": "koehler", "age": 25, "salary": 30000 },
-  { "firstName": "bill", "lastName": "chan", "age": 45, "salary": 40000 },
-  { "firstName": "juan", "lastName": "tanaka", "age": 30, "salary": 50000 }
+  { "id": 1, "firstName": "pepe", "lastName": "smith", "age": 35, "salary": 60000 },
+  { "id": 2, "firstName": "mario", "lastName": "koehler", "age": 25, "salary": 30000 },
+  { "id": 3, "firstName": "bill", "lastName": "chan", "age": 45, "salary": 40000 },
+  { "id": 4, "firstName": "juan", "lastName": "tanaka", "age": 30, "salary": 50000 }
 ]
