9ab242d5a4d7b949f1e45d445b0a3569d8926b2d
jorge <jorgearteaga8936@gmail.com>
2016-01-21 20:46:25 -0400

    init

--- technologies.js
+++
@@ -1,7 +0,0 @@
-[
-  { "name": "Ruby", "likes": 0, "dislikes": 0 },
-  { "name": "Python", "likes": 0, "dislikes": 0 },
-  { "name": "PHP", "likes": 0, "dislikes": 0 },
-  { "name": "Javascript", "likes": 0, "dislikes": 0 },
-  { "name": "Java", "likes": 0, "dislikes": 0 }
-]
